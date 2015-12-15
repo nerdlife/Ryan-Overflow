39e31ef459276117e90b8417cbab8d65a21f11cd
nerdlife <nerdlifeio@gmail.com>
2015-12-14 18:12:39 -0800

    Starting to build votes. Ongoing css condensing

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,34 +1,17 @@
 class VotesController < ApplicationController
 
-   def index
+   def add_vote
+      
    end
-
-   def new
-      @vote = Vote.new
+   
+   def subtract_vote
+      
    end
-
-   def create
-      @vote = Vote.new(vote_params)
-   end
-
-# def edit
-
-# end
-
-# def update
-
-# end
-
-
-# def destroy
-
-# end
-
-
+   
 
 private 
-def vote_params
-   params.require(:vote).permit(:user_id, :question_id, :answer_id)
-end
+   def vote_params
+      params.require(:vote).permit(:user_id, :question_id, :answer_id)
+   end
 
 end
